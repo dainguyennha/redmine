212b56eecd02481c41845e7677350d78e86f6728
dainguyenblabla <dainguyenblabla@gmail.com>
2019-11-21 00:08:15 +0700

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,21 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   # Disable delivery errors
-  config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => "gmail.com",
+      :user_name            => "dainguyenblabla@gmail.com",
+      :password             => "wzapnbboihekoefq",
+      :authentication       => "plain",
+      :enable_starttls_auto => true  }
+#  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  config.action_mailer.default_url_options = { :host => 'kilentic.herokuapp.com' }
+
+  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
   # No email in production log
   config.action_mailer.logger = nil
